3c2d0676779ba5e782f94c5a051898c5e28feb54
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2024-10-23 08:17:05 +0200

    Added VelociCoaster example
    Added VelociCoaster as example coaster

--- RCDbee/RCDbee/ContentView.swift
+++ RCDbee/RCDbee/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  RCDbee
 //
-//  Created by Blindsidesparkles.rectangle.stack on 20.10.24.
+//  Created by Blindside on 20.10.24.
 //
 
 import SwiftUI
@@ -16,9 +16,16 @@ struct ContentView: View {
             }
             return coaster
         }()
-    
     @State var alpengeist: RCDBCoasterDetail? = {
             guard let url = Bundle.main.url(forResource: "Alpengeist", withExtension: "json"),
+                  let data = try? Data(contentsOf: url),
+                  let coaster = try? JSONDecoder().decode(RCDBCoasterDetail.self, from: data) else {
+                return nil
+            }
+            return coaster
+        }()
+    @State var velocicoaster: RCDBCoasterDetail? = {
+            guard let url = Bundle.main.url(forResource: "VelociCoaster", withExtension: "json"),
                   let data = try? Data(contentsOf: url),
                   let coaster = try? JSONDecoder().decode(RCDBCoasterDetail.self, from: data) else {
                 return nil
@@ -31,7 +38,7 @@ struct ContentView: View {
         TabView {
             RandomCoasterView()
                 .tabItem {
-                    Image(systemName: "star.fill")
+                    Image(systemName: "sparkles.rectangle.stack")
                     Text("Random Coaster")
                 }
             CoasterDetailView(coaster: .constant(taron))
@@ -40,6 +47,11 @@ struct ContentView: View {
                     Text("Taron (id:12723)")
                 }
             CoasterDetailView(coaster: .constant(alpengeist))
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Alpengeist (id:277)")
+                }
+            CoasterDetailView(coaster: .constant(velocicoaster))
                 .tabItem {
                     Image(systemName: "star.fill")
                     Text("Alpengeist (id:277)")
