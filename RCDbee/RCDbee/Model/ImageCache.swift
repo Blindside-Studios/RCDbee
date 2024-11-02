bd76dc178a95f243263422bae598ddd81550cdab
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2024-11-02 23:04:30 +0100

    Limited image cacheing
    Only 100 images may be stored on the phone at any given time. This limit may be raised or lowered later. If the limit is reached, the oldest images may be removed from cache.

--- RCDbee/RCDbee/Model/ImageCache.swift
+++ RCDbee/RCDbee/Model/ImageCache.swift
@@ -12,11 +12,17 @@ import SwiftUI
 
 class ImageCache {
     static let shared = ImageCache()
-    private init() {}
+    private init() {
+        loadKeys()
+    }
     
-    // TODO: This is public for debugging purposes, make private again
-    public var cache: [String: UIImage] = [:]
-
+    private var cache: [String: UIImage] = [:]
+    private var keys: [String] = [] // keeping track of saved image order
+    private let limit = 100
+    private let userDefaultsKey = "cachedImageKeys"
+    
+    
+    
     func getImage(forKey key: String) -> UIImage? {
         if let cachedImage = cache[key] {
             return cachedImage
@@ -26,12 +32,7 @@ class ImageCache {
         }
         return nil
     }
-
-    public func saveImage(_ image: UIImage, forKey key: String) {
-        cache[key] = image
-        saveToDisk(image, forKey: key)
-    }
-
+    
     private func loadFromDisk(forKey key: String) -> UIImage? {
         let fileURL = getFileURL(forKey: key)
         if let data = try? Data(contentsOf: fileURL), let image = UIImage(data: data) {
@@ -39,57 +40,85 @@ class ImageCache {
         }
         return nil
     }
-
+    
+    
+    
+    
+    public func saveImage(_ image: UIImage, forKey key: String) {
+        if cache.count >= limit {
+            // Remove the oldest image
+            if let oldestKey = keys.first {
+                cache.removeValue(forKey: oldestKey)
+                removeImageFromDisk(forKey: oldestKey)
+                keys.removeFirst()
+            }
+        }
+        
+        cache[key] = image
+        keys.append(key)
+        saveToDisk(image, forKey: key)
+        saveKeys()
+    }
     
     private func saveToDisk(_ image: UIImage, forKey key: String) {
         let fileURL = getFileURL(forKey: key)
         
-        // Send error message if conversion to JPEG fails
         guard let data = image.jpegData(compressionQuality: 1) else {
             debugPrint("Failed to convert UIImage to JPEG data.")
             return
         }
-        // TODO: This might not always save correctly
         do {
             try data.write(to: fileURL, options: [.atomic])
         } catch {
             debugPrint("Error saving image to \(fileURL): \(error.localizedDescription)")
         }
     }
-
+    
     private func getFileURL(forKey key: String) -> URL {
         let directory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
         return directory.appendingPathComponent(key + ".jpeg")
     }
-
-    private func imageToUIImage(_ image: Image) -> UIImage? {
-        let controller = UIHostingController(rootView: image)
-        let view = controller.view
-
-        let size = view!.intrinsicContentSize
-        view!.bounds = CGRect(origin: .zero, size: size)
-        view!.backgroundColor = .clear
-
-        let renderer = UIGraphicsImageRenderer(size: size)
-        return renderer.image { _ in
-            view!.drawHierarchy(in: view!.bounds, afterScreenUpdates: true)
+    
+    
+    
+    
+    private func removeImageFromDisk(forKey key: String) {
+        let fileURL = getFileURL(forKey: key)
+        do {
+            try FileManager.default.removeItem(at: fileURL)
+        } catch {
+            debugPrint("Failed to remove image from disk: \(error.localizedDescription)")
         }
     }
     
     func clearCache() {
-            cache.removeAll()
-            clearDiskCache()
-        }
-
-        private func clearDiskCache() {
-            let directory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-            do {
-                let fileURLs = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
-                for fileURL in fileURLs {
-                    try FileManager.default.removeItem(at: fileURL)
-                }
-            } catch {
-                print("Could not clear disk cache: \(error)")
+        cache.removeAll()
+        clearDiskCache()
+    }
+    
+    private func clearDiskCache() {
+        let directory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        do {
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: directory, includingPropertiesForKeys: nil)
+            for fileURL in fileURLs {
+                try FileManager.default.removeItem(at: fileURL)
+                keys.removeAll()
+                saveKeys()
             }
+        } catch {
+            print("Could not clear disk cache: \(error)")
         }
+        
+    }
+    
+    
+    private func loadKeys() {
+        if let savedKeys = UserDefaults.standard.array(forKey: userDefaultsKey) as? [String] {
+                keys = savedKeys
+        }
+    }
+
+    private func saveKeys() {
+        UserDefaults.standard.set(keys, forKey: userDefaultsKey)
+    }
 }
